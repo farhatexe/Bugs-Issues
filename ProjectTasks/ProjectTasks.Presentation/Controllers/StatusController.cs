a17706f58251e2c97014b384ef5409bb85eef1d1
M3gur0 <jerome.bolot@gmail.com>
2014-11-19 23:46:13 +0100

    Commit untracked file

---
+++ ProjectTasks/ProjectTasks.Presentation/Controllers/StatusController.cs
@@ -0,0 +1,68 @@
+﻿namespace ProjectTasks.Presentation.Controllers
+{
+    using ProjectTasks.Business.Domain.Entities;
+    using ProjectTasks.Business.Services.Interfaces;
+    using ProjectTasks.Crosscuttings.IoC;
+    using ProjectTasks.Presentation.Core.Bootstrap.Component.DataTables;
+    using ProjectTasks.Presentation.Core.Bootstrap.Extensions;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Handles status features.
+    /// </summary>
+    public class StatusController : Controller
+    {
+        private IServices statusServices = IoCContainer.Default.Resolve<IServices>();
+
+        /// <summary>
+        /// Return default view for status feature.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Gets all available statuses.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>A DataTables result that contains all available status.</returns>
+        [HttpPost]
+        public DataTablesResult<Status> GetAll(DefaultDataTablesRequest request)
+        {
+            return new DataTablesResult<Status>(
+                this.statusServices.GetAllStatus().AsQueryable().ToDataTablesSortedResponse(request), 
+                request, 
+                JsonRequestBehavior.DenyGet);
+        }
+
+        /// <summary>
+        /// Return detail view for status feature.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Detail()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Return edit view for status feature.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Return create view for status feature.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
